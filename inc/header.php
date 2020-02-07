21a3b619a762631d4ac620d75712a13d2ce657b9
Benjamin Plesant <benjamin.plesant@laposte.net>
2020-02-07 16:59:57 +0100

    -menu position fixed -image main -css nav, image main peut-être d'autre choses

--- inc/header.php
+++ inc/header.php
@@ -2,18 +2,29 @@
 <html lang="fr" dir="ltr">
 <head>
     <meta charset="utf-8">
-    <title><?php if(!empty($title)) {echo $title;} else {echo 'nom du site';} ?> </title>
+    <title><?php if (!empty($title)) {echo $title;} else {echo 'nom du site';} ?> </title>
     <link rel="stylesheet" href="asset/css/style.css">
 </head>
 
 
 <body>
 <header>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-        </ul>
+
+    <nav class="wrap">
+        <div class="container-nav" id="logo-nav">
+            <h1><a href="">CV Projekt</a></h1>
+        </div>
+        <div class="liste-nav">
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="index.php">Trouver un CV</a></li>
+                <li><a href="inscription.php">Inscription</a></li>
+                <li><a href="connexion.php">Connexion</a></li>
+            </ul>
+        </div>
     </nav>
+
 </header>
 
-<div id="wrap">
+
+
--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 session_start();
-include ('inc/function.php');
-include ('inc/pdo.php');
-
-
-
+include('inc/function.php');
+include('inc/pdo.php');
 
 
 include('inc/header.php'); ?>
+<main>
+    <div class="wrap" id="image"><img src="asset/img/light-300431_1280.jpg" alt=""></div>
+    <div class="wrap12" id="text-image"><h2>DU TEXTE DU TEXTE DU TEXTE DU TEXTE DU TEXTE DU TEXTE DU TEXTE DU TEXTE</h2></div>
+
+</main>
 
 
 <?php include('inc/footer.php');
