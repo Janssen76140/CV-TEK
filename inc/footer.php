30d3ee25bfcd031bcb4510f0599ba2c2870b9fb3
Sofien <sofiendu76800@gmail.com>
2020-02-27 14:10:33 +0100

    Ajout d'une page sur le footer

--- inc/footer.php
+++ inc/footer.php
@@ -3,6 +3,7 @@
 <footer>
   <a href="index.php">Accueil</a>
   <a href="mentions-legales.php">Mentions légales</a>
+  <a href="cgu.php">Condition générales d'utilisations</a>
   <a href="contact.php">Contact</a>
 </footer>
 
