2434d855de960e65407a36a308461ca67409f789
Etienne Barassin <etiennebarassin@gmail.com>
2020-02-28 11:49:45 +0100

    search bar fonctionnelle

--- index.php
+++ index.php
@@ -3,6 +3,23 @@ session_start();
 require ('inc/header.php');
 require('functions/function.php');
 require ('inc/pdo.php');
+
+if (isset($_GET["submit"]) AND $_GET["submit"] == "Rechercher")
+{
+    $_GET["terme"] = htmlspecialchars($_GET["terme"]);
+    $terme = $_GET["terme"];
+     $terme = trim($terme);
+     $terme = strip_tags($terme);
+}
+if (isset($terme))
+{
+    $terme = strtolower($terme);
+    $sql = "SELECT nom, prenom FROM users_recruteur WHERE nom LIKE ? OR prenom LIKE ? ";
+    $query = $pdo->prepare($sql);
+    $query->execute(array("%".$terme."%", "%".$terme."%"));
+    $recherches = $query->fetchAll();
+    var_dump($recherches);
+}
 ?>
 
 <form action = "index.php" method = "get">
@@ -10,7 +27,6 @@ require ('inc/pdo.php');
     <input type="submit" name="submit" value="Rechercher">
 </form>
 
-<?php
 
 
 <?php
