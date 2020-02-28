e4832766479adc6b01437feb7218ff425c9719cc
Etienne Barassin <etiennebarassin@gmail.com>
2020-02-28 14:51:49 +0100

    import search bar

--- index.php
+++ index.php
@@ -1,11 +1,42 @@
 <?php
-require('vendor/autoload.php');
+session_start();
+require ('inc/header.php');
+require('functions/function.php');
+require ('inc/pdo.php');
 
-use Cocur\Slugify\Slugify;
+if (isset($_GET["submit"]) AND $_GET["submit"] == "Rechercher")
+{
+    $_GET["terme"] = htmlspecialchars($_GET["terme"]);
+    $terme = $_GET["terme"];
 
-$slugify = new Slugify();
-echo $slugify->slugify('Hello World!');
+     $terme = strip_tags($terme);
+}
+if (isset($terme)) {
+    
+    $terme = strtolower($terme);
+    $sql = "SELECT nom, prenom FROM users_recruteur WHERE nom LIKE '%$terme%' OR prenom LIKE '%$terme%' ";
+    $query = $pdo->prepare($sql);
+    $query->execute(array("%$terme%", "%$terme%"));
+    $recherches = $query->fetchAll();
+    var_dump($recherches);
+}
+?>
 
+<form action = "index.php" method = "get">
+    <input type="search" name="terme">
+    <input type="submit" name="submit" value="Rechercher">
+</form>
+
+<?php
+foreach ($recherches as $recherche){
+    if (!isset($recherche)){
+        echo "Il n'y a aucun résultat";
+    } else {
+        echo $recherche['nom'], "<br>" . $recherche['prenom'];
+    }
+}
+
+require ('inc/footer.php');
 
 
 
