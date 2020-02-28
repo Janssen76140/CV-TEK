2cf5ab03cd59f062131aaa6e3db39c627c1a0469
Al-Namrood <$*Acoc7dg2ws*$>
2020-02-28 14:59:31 +0100

    Oui

--- inc/header.php
+++ inc/header.php
@@ -4,25 +4,32 @@
     <meta charset="utf-8">
     <title><?php if (!empty($title)) {echo $title;} else {echo 'nom du site';} ?> </title>
     <link rel="stylesheet" href="asset/css/style.css">
+
 </head>
 
 
-<body>
+<body class="fittext">
 <header>
 
     <nav class="wrap">
-        <div class="container-nav" id="logo-nav">
-            <h1><a href="">CV Projekt</a></h1>
-        </div>
-        <div class="liste-nav">
-            <ul>
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="index.php">Trouver un CV</a></li>
-                <li><a href="inscription.php">Inscription</a></li>
-                <li><a href="connexion.php">Connexion</a></li>
-            </ul>
-        </div>
+        <h1><a href="">CV Projekt</a></h1>
+        <ul>
+
+            <li ><a href="#">Accueil</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+
+            <li class="inscription-nav"><a href="#">Inscription</a></li>
+            <li class="connexion-nav"><a href="#">Connexion</a></li>
+        </ul>
     </nav>
+    <div class="wrap">
+
+        <div class="image-header">
+            <p class="centered">LoremLoremLoremLoremLoremLoremLoremLorem</p>
+        </div>
+    </div>
+
 
 </header>
 
--- index.php
+++ index.php
@@ -1,15 +1,80 @@
 <?php
 session_start();
 include('inc/function.php');
-include('inc/pdo.php');
 
 
 include('inc/header.php'); ?>
-<main>
-    <div class="wrap" id="image"><img src="asset/img/light-300431_1280.jpg" alt=""></div>
-    <div class="wrap12" id="text-image"><h2>DU TEXTE DU TEXTE DU TEXTE DU TEXTE DU TEXTE DU TEXTE DU TEXTE DU TEXTE</h2></div>
-
-</main>
+    <section class="wrap" id="projets-section">
+        <div class="text-section-projet">
+            <h2>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</h2
+            <p>
+                LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem <br>
+                LoremLoremLoremLoremLoremLoremLorem
+            </p>
+        </div>
+        <div class="row">
+            <div class="column">
+                <figure>
+                    <a href=""><img src="asset/images/icon1.png" alt=""></a>
+                    <p>Lorem</p>
+                </figure>
+            </div>
+            <div class="column">
+                <figure>
+                    <a href=""><img src="asset/images/icon2.png" alt=""></a>
+                    <p>Lorem</p>
+                </figure>
+            </div>
+            <div class="column">
+                <figure>
+                    <a href=""> <img src="asset/images/icon3.png" alt=""></a>
+                    <p>Lorem</p>
+                </figure>
+            </div>
+            <div class="column">
+                <figure>
+                    <a href=""><img src="asset/images/icon4.png" alt=""></a>
+                    <p>Lorem</p>
+                </figure>
+            </div>
+            <div class="column">
+                <figure>
+                    <a href=""><img src="asset/images/icon5.png" alt=""></a>
+                    <p>Lorem</p>
+                </figure>
+            </div>
+            <div class="column">
+                <figure>
+                    <a href=""> <img src="asset/images/icon6.png" alt=""></a>
+                    <p>Lorem</p>
+                </figure>
+            </div>
+            <div class="column">
+                <figure>
+                    <a href=""> <img src="asset/images/icon7.png" alt=""></a>
+                    <p>Lorem</p>
+                </figure>
+            </div>
+            <div class="column">
+                <figure>
+                    <a href=""><img src="asset/images/icon8.png" alt=""></a>
+                    <p>Lorem</p>
+                </figure>
+            </div>
+        </div>
+    </section>
+    <section class="wrap2" id="video-section">
+        <div class="text-section-video">
+            <h3>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</h3>
+            <p>
+                LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem <br>
+                LoremLoremLoremLoremLoremLoremLorem
+            </p>
+        </div>
+        <div class='embed-container'>
+            <iframe src='https://www.youtube.com/embed/ah74HeRwXuQ' frameborder='0' allowfullscreen></iframe>
+        </div>
+    </section>
 
 
 <?php include('inc/footer.php');
