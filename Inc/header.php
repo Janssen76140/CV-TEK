dd65b30e9bdfd4a22dce6615d59316f28ef38cde
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2020-03-02 16:57:28 +0100

    Acces au CSS

--- Inc/header.php
+++ Inc/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title><?php if (!empty($title)) {echo $title;} else {echo 'nom du site';} ?> </title>
-    <link rel="stylesheet" href="/asset/css/style.css">
+    <link rel="stylesheet" href="asset/css/style.css">
 
 </head>
 
