194863be2d17045c8a1103db3c566123b5e31eb7
Etienne Barassin <etiennebarassin@gmail.com>
2020-03-03 11:08:35 +0100

    suppression utilisateur admin + html barre de recherche

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,6 @@
 $(window).load(function () {
     /** Adapte la taille de la police en fonction de la taille de l'écran **/
     $(".fittext").fitText(2.5, {minFontSize: '10px', maxFontSize: '20px'});
+
+
 });
--- recherche.php
+++ recherche.php
@@ -18,6 +18,10 @@ $recherches = $search->findWord($getword);
 
 include('Inc/header.php'); ?>
 <h1>CV</h1>
+<form method="get" action="">
+    <input type="search" name="terme" value="">
+    <input type="submit" name="submit" value="Rechercher">
+</form>
 <?php
 foreach ($recherches as $recherche) {
 echo "<div>" . $recherche['nom'], "<br>" . $recherche['prenom'] . "</div>";
