1d084d36d7b382b7ebdd228f9c448e86871d4225
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2020-03-03 09:35:32 +0100

    m

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ include('inc/function.php');
 
     <section class="wrap" id="projets-section">
         <div class="text-section-projet">
-            <h2>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</h2
+            <h2>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem</h2>
             <p>
                 LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem <br>
                 LoremLoremLoremLoremLoremLoremLorem
