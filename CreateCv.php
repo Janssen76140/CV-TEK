8413fdea8e91a2f9b8e192e7803c78208ecca782
Janssen PILLEBOUT <janssen.pillebout@gmail.com>
2020-03-03 12:02:17 +0100

    validation formulaire de la page createcv

--- CreateCv.php
+++ CreateCv.php
@@ -9,22 +9,22 @@ $title = "Création de CV";
 $errors = array();
 $succes = false;
 
-if (!empty($_POST['envoyer'])) {
+if (!empty($_POST['envoyer_cv'])) {
     // FAILLE XSS
-    $titre_cv           = trim(strip_tags($_POST['titre_cv']));
-    $accroche_cv        = trim(strip_tags($_POST['accroche_cv']));
-    $datedebut = trim(strip_tags($_POST['datedebut']));
-    $datefin       = trim(strip_tags($_POST['datefin']));
-    $intitule_poste     = trim(strip_tags($_POST['intitule_poste']));
-    $nom_entreprise         = trim(strip_tags($_POST['nom_entreprise']));
-    $description_experience      = trim(strip_tags($_POST['description_experience']));
-    $date_formation     = trim(strip_tags($_POST['date_formation']));
-    $nom_formation         = trim(strip_tags($_POST['nom_formation']));
-    $nom_ecole     = trim(strip_tags($_POST['nom_ecole']));
-    $ville_ecole         = trim(strip_tags($_POST['ville_ecole']));
+    $titre_cv                   = trim(strip_tags($_POST['titre_cv']));
+    $accroche_cv                = trim(strip_tags($_POST['accroche_cv']));
+    $datedebut                  = trim(strip_tags($_POST['datedebut']));
+    $datefin                    = trim(strip_tags($_POST['datefin']));
+    $intitule_poste             = trim(strip_tags($_POST['intitule_poste']));
+    $nom_entreprise             = trim(strip_tags($_POST['nom_entreprise']));
+    $description_experience     = trim(strip_tags($_POST['description_experience']));
+    $date_formation             = trim(strip_tags($_POST['date_formation']));
+    $nom_formation              = trim(strip_tags($_POST['nom_formation']));
+    $nom_ecole                  = trim(strip_tags($_POST['nom_ecole']));
+    $ville_ecole                = trim(strip_tags($_POST['ville_ecole']));
     $description_formation      = trim(strip_tags($_POST['description_formation']));
-    $centre_interet     = trim(strip_tags($_POST['centre_interet']));
-    $competences         = trim(strip_tags($_POST['competences']));
+    $centre_interet             = trim(strip_tags($_POST['centre_interet']));
+    $competences                = trim(strip_tags($_POST['competences']));
 
     // Validation
     $v = new Validation();
@@ -41,14 +41,14 @@ if (!empty($_POST['envoyer'])) {
     $v->validChamp($errors, $competences, 'competences', 2, 50);
 
 
-    if (count($errors) == 0) {
-        // INSERT into
+    // if (count($errors) == 0) {
+    //     // INSERT into
         
-        $repo = new ArticleRepository;
-        $register = $repo->insertRecruteur($nom,$prenom,$email,$nomEntreprise,$adresse,$telephone,$siret,$password);
-        $success = true;
-        header('Location: connexion.php');
-    }
+    //     $repo = new ArticleRepository;
+    //     $register = $repo->insertRecruteur($nom,$prenom,$email,$nomEntreprise,$adresse,$telephone,$siret,$password);
+    //     $success = true;
+    //     header('Location: connexion.php');
+    // }
 }
 
 $form = new Form($errors);
@@ -61,9 +61,9 @@ include('inc/header.php'); ?>
 
     <h3>Titres et accroche</h3>
 
-    <?= $form->label('titrecv', 'Titre du CV'); ?>
-    <?= $form->input('titrecv', 'text'); ?>
-    <?= $form->errors('titrecv'); ?>
+    <?= $form->label('titre_cv', 'Titre du CV'); ?>
+    <?= $form->input('titre_cv', 'text'); ?>
+    <?= $form->errors('titre_cv'); ?>
 
     <?= $form->label('accroche_cv', 'Accroche du CV'); ?>
     <?= $form->textarea('accroche_cv'); ?>
@@ -80,57 +80,57 @@ include('inc/header.php'); ?>
     <?= $form->optionYear('datefin', '2020'); ?>
     <?= $form->errors('datedebut'); ?>
 
-    <?= $form->label('intitule-poste', 'Intitulé du poste'); ?>
-    <?= $form->input('intitule-poste', 'text'); ?>
-    <?= $form->errors('intitule-poste'); ?>
+    <?= $form->label('intitule_poste', 'Intitulé du poste'); ?>
+    <?= $form->input('intitule_poste', 'text'); ?>
+    <?= $form->errors('intitule_poste'); ?>
 
-    <?= $form->label('nom-entreprise', 'Nom de l\'entreprise'); ?>
-    <?= $form->input('nom-entreprise', 'text'); ?>
-    <?= $form->errors('nom-entreprise'); ?>
+    <?= $form->label('nom_entreprise', 'Nom de l\'entreprise'); ?>
+    <?= $form->input('nom_entreprise', 'text'); ?>
+    <?= $form->errors('nom_entreprise'); ?>
 
-    <?= $form->label('description-experience', 'Description de votre éxperience'); ?>
-    <?= $form->textarea('description-experience'); ?>
-    <?= $form->errors('description-experience'); ?>
+    <?= $form->label('description_experience', 'Description de votre éxperience'); ?>
+    <?= $form->textarea('description_experience'); ?>
+    <?= $form->errors('description_experience'); ?>
 
 
     <h3>Formations</h3>
 
-    <?= $form->label('date-formation', 'Date de fin de la formation'); ?>
-    <?= $form->optionYear('date-formation', '2020'); ?>
-    <?= $form->errors('date-formation'); ?>
+    <?= $form->label('date_formation', 'Date de fin de la formation'); ?>
+    <?= $form->optionYear('date_formation', '2020'); ?>
+    <?= $form->errors('date_formation'); ?>
 
-    <?= $form->label('nom-formation', 'Intitulé de la formation'); ?>
-    <?= $form->input('nom-formation', 'text'); ?>
-    <?= $form->errors('nom-formation'); ?>
+    <?= $form->label('nom_formation', 'Intitulé de la formation'); ?>
+    <?= $form->input('nom_formation', 'text'); ?>
+    <?= $form->errors('nom_formation'); ?>
 
-    <?= $form->label('nom-ecole', 'Nom de votre école'); ?>
-    <?= $form->input('nom-ecole', 'text'); ?>
-    <?= $form->errors('nom-ecole'); ?>
+    <?= $form->label('nom_ecole', 'Nom de votre école'); ?>
+    <?= $form->input('nom_ecole', 'text'); ?>
+    <?= $form->errors('nom_ecole'); ?>
 
-    <?= $form->label('ville-ecole', 'Ville de votre école'); ?>
-    <?= $form->input('ville-ecole', 'text'); ?>
-    <?= $form->errors('ville-ecole'); ?>
+    <?= $form->label('ville_ecole', 'Ville de votre école'); ?>
+    <?= $form->input('ville_ecole', 'text'); ?>
+    <?= $form->errors('ville_ecole'); ?>
 
-    <?= $form->label('description-formation', 'Description de votre formation'); ?>
-    <?= $form->textarea('description-formation'); ?>
-    <?= $form->errors('description-formation'); ?>
+    <?= $form->label('description_formation', 'Description de votre formation'); ?>
+    <?= $form->textarea('description_formation'); ?>
+    <?= $form->errors('description_formation'); ?>
 
 
     <h3>Centre d'intérêt</h3>
 
-    <?= $form->label('centre-interet', 'Vos centres d\'intérêt'); ?>
-    <?= $form->input('centre-interet', 'text'); ?>
-    <?= $form->errors('centre-interet'); ?>
+    <?= $form->label('centre_interet', 'Vos centres d\'intérêt'); ?>
+    <?= $form->input('centre_interet', 'text'); ?>
+    <?= $form->errors('centre_interet'); ?>
 
 
     <h3>Compétences</h3>
 
-    <?= $form->label('competence', 'Vos compétences'); ?>
-    <?= $form->input('competence', 'text'); ?>
-    <?= $form->errors('competence'); ?>
+    <?= $form->label('competences', 'Vos compétences'); ?>
+    <?= $form->input('competences', 'text'); ?>
+    <?= $form->errors('competences'); ?>
 
 
-    <?= $form->submit('envoyer','Envoyer le CV'); ?>
+    <?= $form->submit('envoyer_cv','Envoyer le CV'); ?>
 </form>
 
 <?php include('inc/footer.php');
