5b87b30cff8777337a5d3b19c007ab6929ecd218
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2020-03-03 11:18:25 +0100

    Commit : -Correction du header et continuation du CSS

--- Inc/header.php
+++ Inc/header.php
@@ -14,16 +14,10 @@
         <h1><a href="">CV Projekt</a></h1>
         <ul>
             <li><a href="index.php">Accueil</a></li>
-<<<<<<< HEAD
             <li><a href="#">Services</a></li>
             <li><a href="#">Contact</a></li>
             <li><a href="CreateCv.php">Creation CV</a></li>
             <li class="inscription-nav"><a href="#">Inscription</a></li>
-=======
-            <li><a href="#projets-section">Services</a></li>
-            <li><a href="#contact-section">Contact</a></li>
-            <li class="inscription-nav"><a href="inscription.php">Inscription</a></li>
->>>>>>> 3aa062b8140238263a854adbf6b91331bb6a89f6
             <li class="connexion-nav"><a href="connexion.php">Connexion</a></li>
             <li><a href="admin/index.php">Pannel admin</a></li>
         </ul>
