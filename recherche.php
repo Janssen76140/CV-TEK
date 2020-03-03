80c4a6e5a36b77ba6aa8345c65709d3db154dd9c
Al-Namrood <$*Acoc7dg2ws*$>
2020-03-03 12:22:24 +0100

    intégration barre de recherche

--- recherche.php
+++ recherche.php
@@ -17,11 +17,7 @@ $recherches = $search->findWord($getword);
 
 
 include('Inc/header.php'); ?>
-<h1>CV</h1>
-<form method="get" action="">
-    <input type="search" name="terme" value="">
-    <input type="submit" name="submit" value="Rechercher">
-</form>
+
 <?php
 foreach ($recherches as $recherche) {
 echo "<div>" . $recherche['nom'], "<br>" . $recherche['prenom'] . "</div>";
