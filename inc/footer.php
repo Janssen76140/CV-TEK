ee76ca276dcecc3570316dc87a4324365b9f766f
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2020-03-04 10:35:34 +0100

    Footer

--- inc/footer.php
+++ inc/footer.php
@@ -8,17 +8,16 @@
         <a href="#" id="twitter"  class="fa fa-twitter"></a>
     </div>
     <ul>
-        <li><a href="cgu.php">CGU</a></li>
+        <li><a href="cgu.php">CGU   |   </a></li>
         <li><a href="mentions-legales.php">Mentions légales</a></li>
     </ul>
 </footer>
 
 
-<script src="https://code.jquery.com/jquery-2.2.4.min.js"
-        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
 <script src="asset/fittext/jquery.fittext.js"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 <script type="text/javascript" src="asset/js/main.js"></script>
 
 </body>
